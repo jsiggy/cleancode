ea1fb32e2acce5c68a05d434c6ce842a1cc285a9
john.sigler <john.sigler@gmail.com>
2024-01-13 19:21:58 -0600

    Refactor tests

--- src/test/java/com/siggy/training/code/smells/calculator/ArithmeticCalculatorTest.java
+++ src/test/java/com/siggy/training/code/smells/calculator/ArithmeticCalculatorTest.java
@@ -1,40 +1,49 @@
 package com.siggy.training.code.smells.calculator;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ArithmeticCalculatorTest {
+
+    private ArithmeticCalculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new ArithmeticCalculator();
+    }
+
     @Test
     void canAdd() {
-        assertEquals(35, new ArithmeticCalculator().add(20, 15));
+        assertEquals(35, calculator.add(20, 15));
     }
 
     @Test
     void canSubtract() {
-        assertEquals(-12, new ArithmeticCalculator().subtract(75, 87));
+        assertEquals(-12, calculator.subtract(75, 87));
     }
 
     @Test
     void canMultiply() {
-        assertEquals(3577, new ArithmeticCalculator().multiply(49, 73));
+        assertEquals(3577, calculator.multiply(49, 73));
     }
 
     @Test
     void canDivide() {
-        assertEquals(5, new ArithmeticCalculator().divide(80, 16));
+        assertEquals(5, calculator.divide(80, 16));
     }
 
     @Test
     void shouldFailSine() {
         assertThrows(UnsupportedOperationException.class,
-                ()->new ArithmeticCalculator().sin(0.0));
+                ()-> calculator.sin(0.0));
     }
 
     @Test
     void shouldFailCosine() {
         assertThrows(UnsupportedOperationException.class,
-                ()->new ArithmeticCalculator().cos(0.0));
+                ()-> calculator.cos(0.0));
     }
 }
--- src/test/java/com/siggy/training/code/smells/calculator/ScientificCalculatorTest.java
+++ src/test/java/com/siggy/training/code/smells/calculator/ScientificCalculatorTest.java
@@ -1,29 +1,36 @@
 package com.siggy.training.code.smells.calculator;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ScientificCalculatorTest {
     public static final double DEGREES_TO_RADIANS = Math.PI / 180.0;
+    private ScientificCalculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new ScientificCalculator();
+    }
 
     @Test
     void canCalculateSine() {
-        assertEquals(0.0, new ScientificCalculator().sin(0.0 * DEGREES_TO_RADIANS), 0.001);
-        assertEquals(0.5, new ScientificCalculator().sin(30.0 * DEGREES_TO_RADIANS), 0.001);
-        assertEquals(0.7071, new ScientificCalculator().sin(45.0 * DEGREES_TO_RADIANS), 0.001);
-        assertEquals(0.866, new ScientificCalculator().sin(60.0 * DEGREES_TO_RADIANS), 0.001);
-        assertEquals(1.0, new ScientificCalculator().sin(90.0 * DEGREES_TO_RADIANS), 0.001);
-        assertEquals(0.0, new ScientificCalculator().sin(180.0 * DEGREES_TO_RADIANS), 0.001);
+        assertEquals(0.0, calculator.sin(0.0 * DEGREES_TO_RADIANS), 0.001);
+        assertEquals(0.5, calculator.sin(30.0 * DEGREES_TO_RADIANS), 0.001);
+        assertEquals(0.7071, calculator.sin(45.0 * DEGREES_TO_RADIANS), 0.001);
+        assertEquals(0.866, calculator.sin(60.0 * DEGREES_TO_RADIANS), 0.001);
+        assertEquals(1.0, calculator.sin(90.0 * DEGREES_TO_RADIANS), 0.001);
+        assertEquals(0.0, calculator.sin(180.0 * DEGREES_TO_RADIANS), 0.001);
     }
 
     @Test
     void canCalculateCosine() {
-        assertEquals(1.0, new ScientificCalculator().cos(0.0 * DEGREES_TO_RADIANS), 0.001);
-        assertEquals(0.866, new ScientificCalculator().cos(30.0 * DEGREES_TO_RADIANS), 0.001);
-        assertEquals(0.7071, new ScientificCalculator().cos(45.0 * DEGREES_TO_RADIANS), 0.001);
-        assertEquals(0.5, new ScientificCalculator().cos(60.0 * DEGREES_TO_RADIANS), 0.001);
-        assertEquals(0.0, new ScientificCalculator().cos(90.0 * DEGREES_TO_RADIANS), 0.001);
-        assertEquals(-1.0, new ScientificCalculator().cos(180.0 * DEGREES_TO_RADIANS), 0.001);
+        assertEquals(1.0, calculator.cos(0.0 * DEGREES_TO_RADIANS), 0.001);
+        assertEquals(0.866, calculator.cos(30.0 * DEGREES_TO_RADIANS), 0.001);
+        assertEquals(0.7071, calculator.cos(45.0 * DEGREES_TO_RADIANS), 0.001);
+        assertEquals(0.5, calculator.cos(60.0 * DEGREES_TO_RADIANS), 0.001);
+        assertEquals(0.0, calculator.cos(90.0 * DEGREES_TO_RADIANS), 0.001);
+        assertEquals(-1.0, calculator.cos(180.0 * DEGREES_TO_RADIANS), 0.001);
     }
 }
