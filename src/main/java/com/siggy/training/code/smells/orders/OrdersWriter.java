bc1ac9d2ec86d86a6117b55564022ceafaf33c44
john.sigler <john.sigler@gmail.com>
2024-01-15 20:00:10 -0600

    Add a main() method to OrderWriters

--- src/main/java/com/siggy/training/code/smells/orders/OrdersWriter.java
+++ src/main/java/com/siggy/training/code/smells/orders/OrdersWriter.java
@@ -1,7 +1,7 @@
 package com.siggy.training.code.smells.orders;
 
 public class OrdersWriter {
-    private Orders orders;
+    private final Orders orders;
 
     public OrdersWriter(Orders orders) {
         this.orders = orders;
@@ -57,5 +57,24 @@ public class OrdersWriter {
     private String getSizeFor(Product product) {
         var sizeString = product.getSize().toString().toLowerCase();
         return product.getSize() == ProductSize.NOT_APPLICABLE ? "" : sizeString;
+    }
+
+    public static void main(String[] args) {
+        Product p1005 = new Product("p1005", "Blah 1", Color.WHITE, 10.15F);
+        Product t8934 = new Product("t8934", "Blah 2", Color.YELLOW, 12.50F, ProductSize.SMALL);
+        Product r9075 = new Product("r9075", "My product 1", Color.RED, 15.99F, ProductSize.MEDIUM);
+        Product t0666 = new Product("t0666", "My product 2", Color.PINK, 200.93F, ProductSize.LARGE);
+        Product s1231 = new Product("s1231", "My product 3", Color.RED, 1.50F);
+        Order id1 = new Order("id1");
+        id1.addProduct(p1005);
+        id1.addProduct(t8934);
+        Order id2 = new Order("id2");
+        id2.addProduct(r9075);
+        id2.addProduct(t0666);
+        id2.addProduct(s1231);
+        Orders orders = new Orders();
+        orders.addOrder(id1);
+        orders.addOrder(id2);
+        System.out.println(new OrdersWriter(orders).getContents());
     }
 }
